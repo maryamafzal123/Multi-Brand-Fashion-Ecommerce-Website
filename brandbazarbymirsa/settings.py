0eb01014e40c4839f7fb1c63e462629b843bd7df
Maryam Afzal <maryamAfzal@gmail.com>
2026-04-18 21:17:24 +0500

    Add gunicorn to requirements

--- brandbazarbymirsa/settings.py
+++ brandbazarbymirsa/settings.py
@@ -123,3 +123,9 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
+
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET'),
+)
