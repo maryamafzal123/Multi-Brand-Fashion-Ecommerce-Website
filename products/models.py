074ce519da81905a0f33c5c891ef1aaf5c4891db
Maryam Afzal <maryamAfzal@gmail.com>
2026-04-24 22:42:44 +0500

    Fix slug auto increment - Product and Category

--- products/models.py
+++ products/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils.text import slugify
-from cloudinary_storage.storage import MediaCloudinaryStorage
 from cloudinary_storage.storage import RawMediaCloudinaryStorage
-
 from django.core.exceptions import ValidationError
+
 
 def validate_file_size(value):
     limit = 10 * 1024 * 1024  # 10MB
     if value.size > limit:
         raise ValidationError(f'File too large! Max size is 10MB. Your file is {round(value.size/1024/1024, 1)}MB')
-    
+
+
 class Category(models.Model):
     name       = models.CharField(max_length=100, unique=True)
     slug       = models.SlugField(unique=True, db_index=True)
@@ -28,7 +28,13 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Category.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
 
@@ -71,7 +77,13 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
     @property
@@ -91,10 +103,10 @@ class ProductImage(models.Model):
         related_name='images', db_index=True
     )
     image = models.FileField(
-    upload_to='products/',
-    storage=RawMediaCloudinaryStorage(),
-    validators=[validate_file_size]
-)
+        upload_to='products/',
+        storage=RawMediaCloudinaryStorage(),
+        validators=[validate_file_size]
+    )
     is_primary = models.BooleanField(default=False)
     order      = models.PositiveSmallIntegerField(default=0)
 
@@ -105,15 +117,12 @@ class ProductImage(models.Model):
         ]
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(self.name)
-            slug = base_slug
-            counter = 1
-            while Product.objects.filter(slug=slug).exclude(pk=self.pk).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
+        if self.is_primary:
+            ProductImage.objects.filter(
+                product=self.product, is_primary=True
+            ).exclude(pk=self.pk).update(is_primary=False)
         super().save(*args, **kwargs)
+
 
 class ProductVariant(models.Model):
     product     = models.ForeignKey(
