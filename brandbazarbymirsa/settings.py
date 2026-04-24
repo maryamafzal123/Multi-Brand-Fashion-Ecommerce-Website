07acd2935aaa01e9cd6b86dd3c4b10d440be3ba0
Maryam Afzal <maryamAfzal@gmail.com>
2026-04-24 23:58:04 +0500

    Change page size to 20

--- brandbazarbymirsa/settings.py
+++ brandbazarbymirsa/settings.py
@@ -74,7 +74,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 12,
+    'PAGE_SIZE': 20,
 }
 
 SIMPLE_JWT = {
